d2d730a871e5d4371fa1f0c6de923f7b56b8bef8
Jimi <jimiherken@icloud.com>
2023-04-05 20:04:39 +0200

    ➕ Added navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,12 +1,112 @@
 <script lang="ts">
-    import {each} from "svelte/internal";
-
     export let items: { text: string, url: string }[];
-    export let selected: number;
+    export let right: { name: string, image: string, items: { text: string, url: string }[]};
+    let menuHidden = true;
 </script>
 
 <html>
-    {#each items as item, i}
-        <a href="{item.url}" class:selected={i === selected} on:click={() => selected = i}>{item.text}</a>
-    {/each}
+    <ul id="navbar">
+        {#each items as item}
+            <li on:click={window.location.href = item.url}><a>{item.text}</a></li>
+        {/each}
+
+        {#if right && right.name === 'menu'}
+            <li class="right" on:click={() => menuHidden = !menuHidden}>
+                <img src={right.image} alt="Profile image">
+                <ul class="menu" class:hidden={menuHidden}>
+                    {#each right.items as item}
+                        <li class="menu-item" on:click={window.location.href = item.url}><a>{item.text}</a></li>
+                    {/each}
+                </ul>
+            </li>
+        {/if}
+    </ul>
 </html>
+
+<style lang="scss">
+    @use 'src/vars';
+
+    ul#navbar {
+      background-color: vars.$background-color;
+      list-style-type: none;
+      margin: 0;
+      display: flex;
+      flex-direction: row;
+      padding: 0;
+      user-select: none;
+
+      li {
+        padding: 10px 20px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &:hover {
+          background-color: vars.$background-color-light;
+          cursor: pointer;
+        }
+
+        a {
+          color: white;
+          text-decoration: none;
+          height: 100%;
+          width: 100%;
+          font-family: vars.$ubuntu;
+          font-size: 1.15rem;
+        }
+
+        &.right {
+          padding: 5px 5px;
+          margin-left: auto;
+
+          &:hover {
+            background-color: transparent;
+          }
+
+          img {
+            height: 2em;
+            border-radius: 50%;
+          }
+        }
+      }
+    }
+
+    ul.menu {
+      position: absolute;
+      top: 2.6em;
+      right: 0;
+      background-color: #aa0022;
+      list-style-type: none;
+      margin: 0;
+      display: flex;
+      flex-direction: column;
+      padding: 0;
+      user-select: none;
+      z-index: 1;
+
+      li.menu-item {
+        padding: 10px 20px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &:hover {
+          background-color: vars.$background-color-light;
+          cursor: pointer;
+        }
+
+        a {
+          color: white;
+          text-decoration: none;
+          height: 100%;
+          width: 100%;
+          font-family: vars.$ubuntu;
+          font-size: 1.15rem;
+        }
+      }
+
+      &.hidden {
+        display: none;
+      }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,34 @@
 <script>
     import Navbar from "$lib/Navbar.svelte";
+
+    let imageUrl = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg";
 </script>
 
-<html>
+<html lang="en">
     <head>
-        <title>My App</title>
+        <title>Cranberry</title>
     </head>
     <body>
-        <Navbar items={[{text: "Test", url: "https://google.com"}]} selected={0}/>
+        <Navbar
+            items={[
+                {text: "Test", url: "https://google.com"},
+                {text: "Test2", url: "https://jimii.xyz"}
+            ]}
+            right={{
+                name: "menu",
+                image: imageUrl,
+                items: [
+                    {text: "Test", url: "https://google.com"},
+                    {text: "Test2", url: "https://jimii.xyz"}
+                ]
+            }}
+        />
     </body>
 </html>
+
+<style lang="scss">
+    body {
+        margin: 0;
+        padding: 0;
+    }
+</style>
