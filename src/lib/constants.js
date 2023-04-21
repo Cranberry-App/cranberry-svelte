e7e5ad4b0565af0f5e1a05df3f3daa91a203a672
Jimi <jimiherken@icloud.com>
2023-04-21 13:50:59 +0200

    ➕ Added constants

---
+++ src/lib/constants.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDQNePdctttHohZyI4p3C2Hbuax_UZOwcQ",
+  authDomain: "cranberry-chatapp.firebaseapp.com",
+  projectId: "cranberry-chatapp",
+  storageBucket: "cranberry-chatapp.appspot.com",
+  messagingSenderId: "759693558673",
+  appId: "1:759693558673:web:a4ed50b109bdb39b789030",
+  measurementId: "G-KQF11BDQLS"
+};
